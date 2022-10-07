7f91d7323ddc915282ccf74d353b12bcf58d9ae9
An Vong <anvong@gmail.com>
2022-10-07 10:58:19 +0930

    Refactory game class and unit test case with OOP concept

--- RockPaperScissorGame.py
+++ RockPaperScissorGame.py
@@ -2,89 +2,170 @@
 import random
 
 
-def find_winner_choice(player, computer):
-    """Do find the winner base on a defined rule."""
+class RockPaperScissor():
+    """The game object of Rock Paper Scissor."""
+
     possible_choice = ["rock", "paper", "scissor"]
-
-    if player == computer and (player in possible_choice):
-        return "draw"
-    elif (player == "paper" and computer == "rock") \
-            or (player == "rock" and computer == "scissor") \
-            or (player == "scissor" and computer == "paper"):
-        return "player"
-    elif (computer == "paper" and player == "rock") \
-            or (computer == "rock" and player == "scissor") \
-            or (computer == "scissor" and player == "paper"):
-        return "computer"
-    else:
-        raise ValueError
-
-
-def new_game():
-    """Do start a new game."""
-    result = ""
-    match_round = 1
     player_score = 0
     computer_score = 0
-    player = "rock"
-    computer = "paper"
-    possible_choice = ["rock", "paper", "scissor"]
-    exit_game = False
+    match_round = 1
+    max_round = 5
+    winner_point = 5
 
-    # do loop until player or computer get score = 5
-    while (player_score < 5 and computer_score < 5) and not exit_game:
-        print("\nRound ", match_round, " begin:")
-        # validate input loop #
-        while True:
-            # get user input by a number associated with a choice
-            player_input = input(
-                "Please make your choice: 1. rock, 2. paper, 3. scissor, 4. Exit game? ")  # noqa
-            # make the loop to accept only particular values defined 1,2,3
-            if (player_input == "1" or player_input == "2" or player_input == "3"):  # noqa
-                break
-            elif player_input == "4":
-                # player enter 4 to exit the game
-                exit_game = True
-                break
-            else:
-                print("Invalid input. Please try again!")
+    def __init__(self):
+        """Class constructer."""
+        self.player_score = 0
+        self.computer_score = 0
+        self.match_round = 1
+        self.max_round = 5
 
-        # if user not exit game, find the winner of the entire round
-        if not exit_game:
-            player = possible_choice[int(player_input)-1]
-            computer = random.choice(possible_choice)
-            # find the winner choice between player and computer
-            result = find_winner_choice(player, computer)
-            if (result == "player"):
-                player_score += 1
-                result = "you won"
-            elif (result == "computer"):
-                computer_score += 1
-                result = "computer won"
+    def find_winner_choice(self, player, computer):
+        """Do find the winner base on a defined rule."""
+        player = str(player).lower()
+        computer = str(computer).lower()
+        if player not in self.possible_choice:
+            raise ValueError
+        if computer not in self.possible_choice:
+            raise ValueError
 
-            # print("Round ", match_round, "result: your choice:",
-            #     player, ", computer's choice:", computer, "\nResult:",
-            #     result, "\tPlayer Score:",
-            #     player_score, "\tComputer Score:",
-            #         computer_score)  # noqa
-            print(
-                "You chose [{player}] vs Computer [{computer}]"
-                .format(player=player, computer=computer))
-            print("Round {round} result: {result}.".format(
-                round=match_round, result=result), end=" ")
-            print("Player Score: {player_score}".format(
-                player_score=player_score), end=" ")
-            print("tComputer Score: {computer_score}".format(
-                computer_score=computer_score))
-            # increase match round for next loop
-            match_round += 1
+        if player == computer:
+            return "draw"
+        elif (player == "paper" and computer == "rock") \
+                or (player == "rock" and computer == "scissor") \
+                or (player == "scissor" and computer == "paper"):
+            return "player"
+        elif (computer == "paper" and player == "rock") \
+                or (computer == "rock" and player == "scissor") \
+                or (computer == "scissor" and player == "paper"):
+            return "computer"
 
-    if player_score == computer_score:
-        print("Draw match and no winner!")
-    elif player_score > computer_score:
-        print("Congratulations! You won ;)")
-    else:
-        print("Game over! You lose :(")
+    def new_game(self, max_round=5):
+        """Do start a new game."""
+        # reset game info
+        self.reset()
+        # exit game flag
+        exit_game = False
+
+        # do loop until player or computer get score = 5
+        while (not self.get_game_winner()) and not exit_game:
+            # print("\nRound ", self.match_round, " begin:")
+            print("\nRound {round} begin:".format(round=self.match_round))
+            # validate input loop #
+            while True:
+                # get user input by a number associated with a choice
+                player_input = input(
+                    "Please make your choice: 1. ROCK, 2. PAPER, 3. SCISSOR, 0. Exit game? ")  # noqa
+                # make the loop to accept only particular values defined 1,2,3
+                if (player_input == "1" or player_input == "2" or player_input == "3"):  # noqa
+                    break
+                elif player_input == "0":
+                    # player enter 4 to exit the game
+                    exit_game = True
+                    break
+                else:
+                    print("Invalid input. Please try again!")
+
+            # if user not exit game, find the winner of the entire round
+            if not exit_game:
+                player = self.get_choice_name(player_input)
+                # computer pick a random choice
+                computer = self.get_random_choice()
+                # judge for who is winner for this round by the choices
+                round_winner = self.find_winner_choice(player, computer)
+                # set score for the winner of current round
+                self.set_round_score(round_winner)
+                # print round result
+                self.print_round_result(player, computer, round_winner)
+                # call next round
+                self.next_round()
+
+        if exit_game:
+            # print message when user quit the game
+            print("THE MATCH IS WIDTHRAW!")
+        else:
+            # get the game winner
+            final_winner = self.get_game_winner()
+            if final_winner == "player":
+                # message for player is winner
+                print("CONGRATSTULATION!!! YOU WON THE GAME.")
+            elif final_winner == "computer":
+                # message for player is loser
+                print("SORRY MATE!!! YOU LOSE.")
+
+    def get_random_choice(self):
+        """Get a random choice."""
+        return random.choice(self.possible_choice)
+
+    def get_choice_name(self, enter_key):
+        """Get choice name from an input key."""
+        enter_key = str(enter_key)
+        if (enter_key == "1" or enter_key == "2" or enter_key == "3"):
+            return self.possible_choice[int(enter_key)-1]
+        else:
+            raise ValueError
+
+    def set_round_score(self, winner):
+        """Set round result score function."""
+        winner = str(winner).lower()
+        if winner == "player":
+            self.player_score += 1
+        elif winner == "computer":
+            self.computer_score += 1
+        elif winner == "draw":
+            pass  # no points for both
+        else:
+            raise ValueError
+        return True
+
+    def next_round(self):
+        """Increase the match round."""
+        self.match_round += 1
+
+    def get_game_winner(self):
+        """Get game result base on current score after max_round."""
+        if self.match_round < self.max_round:
+            return None
+        if self.computer_score < self.winner_point \
+           and self.player_score < self.winner_point:
+            return None
+        if self.player_score == self.computer_score:
+            return "draw"
+        elif self.player_score < self.computer_score:
+            return "computer"
+        elif self.player_score > self.computer_score:
+            return "player"
+
+    def print_round_result(self, player, computer, winner):
+        """Do print round result."""
+        if player not in self.possible_choice or \
+                computer not in self.possible_choice:
+            raise ValueError
+        if winner not in ["draw", "player", "computer"]:
+            raise ValueError
+
+        # print user vs computer choice
+        print("You chose [{player}] vs Computer [{computer}]. "
+              .format(player=player.upper(),
+                      computer=computer.upper()), end="")
+        # print who is winner of this round
+        if winner == "draw":
+            print("Round {round} is draw. ".format(
+                round=self.match_round), end="")
+        else:
+            print("Round {round} {winner} won. ".format(
+                round=self.match_round, winner=winner), end="")
+
+        print("Player Score: {player_score}".format(
+            player_score=self.player_score), end=", ")
+        print("Computer Score: {computer_score}".format(
+            computer_score=self.computer_score))
+        return True
+
+    def reset(self):
+        """Reset game data."""
+        self.match_round = 1
+        self.player_score = 0
+        self.computer_score = 0
 
 
 if __name__ == '__main__':
@@ -98,14 +179,15 @@ if __name__ == '__main__':
           "paper vs scissor->scissor wins\n")
 
     # start first game
-    new_game()
+    game = RockPaperScissor()
+    game.new_game()
     # do loop to ask user to restart with a new game
     while True:
         response = input(
             "\nDo you want to play again? Enter Y to restart, other key to exit... ")  # noqa
         if response.lower() == "yes" or response.lower() == "y":  # noqa
             # start game again
-            new_game()
+            game.new_game()
         else:
             print("Thank you! See you soon.")
             break
--- TestRockPaperScissorGame.py
+++ TestRockPaperScissorGame.py
@@ -1,6 +1,6 @@
 """Unit test for Rock Paper Scissor game."""
 import unittest
-from RockPaperScissorGame import find_winner_choice
+from RockPaperScissorGame import RockPaperScissor
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,41 +8,250 @@ class MyTestCase(unittest.TestCase):
 
     def test_find_winner_choice_player_win(self):
         """Do test player is the winner."""
-        player, computer = "paper", "rock"
+        game_object = RockPaperScissor()
+        # winner is player
         winner = "player"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        # player paper win
+        player, computer = "paper", "rock"
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
+        # player rock win
         player, computer = "rock", "scissor"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
+        # player scissor win -
         player, computer = "scissor", "paper"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
 
     def test_find_winner_choice_computer_win(self):
         """Do test computer is the winner."""
-        computer, player = "paper", "rock"
+        # start game class
+        game_object = RockPaperScissor()
+        # define winner
         winner = "computer"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        # computer is winner with paper
+        computer, player = "paper", "rock"
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
+        # computer is winner with rock
         computer, player = "rock", "scissor"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
+        # computer is winner with scissor
         computer, player = "scissor", "paper"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
 
     def test_find_winner_choice_draw_result(self):
         """Do test function find winner choice draw result."""
+        # start game class
+        game_object = RockPaperScissor()
         winner = "draw"
         player, computer = "rock", "rock"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
         player, computer = "scissor", "scissor"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
         player, computer = "paper", "paper"
-        self.assertEqual(winner, find_winner_choice(player, computer))
+        self.assertEqual(
+            winner, game_object.find_winner_choice(player, computer))
 
     def test_find_winner_choice_invalid_input(self):
         """Do test invalid input for find winner choice."""
-        self.assertRaises(ValueError, find_winner_choice, "Nothing", "rock")
-        self.assertRaises(ValueError, find_winner_choice, 1, 2)
-        self.assertRaises(ValueError, find_winner_choice, "scissor", " ")
+        # start game class
+        game_object = RockPaperScissor()
+        # assert the error
+        self.assertRaises(
+            ValueError, game_object.find_winner_choice, "Nothing", "rock")
+        self.assertRaises(ValueError, game_object.find_winner_choice, 1, 2)
+        self.assertRaises(
+            ValueError, game_object.find_winner_choice, "scissor", " ")
         with self.assertRaises(ValueError):
-            find_winner_choice("rock1", "rock2")
+            game_object.find_winner_choice("rock1", "rock2")
+
+    def test_get_random_choice_ok(self):
+        """Test random choice OK case."""
+        # possible choices
+        possible_choice = ["rock", "paper", "scissor", ]
+        # start game class
+        game_object = RockPaperScissor()
+        # test randomize choice given by game class
+        pc = game_object.get_random_choice()
+        self.assertIn(pc, possible_choice)
+        pc = game_object.get_random_choice()
+        self.assertIn(pc, possible_choice)
+
+    def test_get_choice_name(self):
+        """Test the function returning correct choice from a key."""
+        # possible_choice = ["rock", "paper", "scissor", ]
+        # start game class
+        game_object = RockPaperScissor()
+        # get rock with key 1
+        self.assertEqual(
+            "rock", game_object.get_choice_name("1"))
+        # get paper with key 2
+        self.assertEqual(
+            "paper", game_object.get_choice_name("2"))
+        # get scissor with key 3
+        self.assertEqual(
+            "scissor", game_object.get_choice_name("3"))
+
+    def test_get_choice_name_invalid_key(self):
+        """Test the error will be raise by input invalid ley."""
+        # possible_choice = ["rock", "paper", "scissor", ]
+        # start game class
+        game_object = RockPaperScissor()
+        # choice key with invalid input will raise error
+        self.assertRaises(ValueError, game_object.get_choice_name, 0)
+        self.assertRaises(ValueError, game_object.get_choice_name, "Something")
+        self.assertRaises(ValueError, game_object.get_choice_name, None)
+        self.assertRaises(ValueError, game_object.get_choice_name, " ")
+        self.assertRaises(ValueError, game_object.get_choice_name, "0")
+        self.assertRaises(ValueError, game_object.get_choice_name, "99")
+
+    def test_set_round_score(self):
+        """Test the function set the round score."""
+        # start game class
+        game_object = RockPaperScissor()
+        # test set score by winner with
+        self.assertTrue(game_object.set_round_score("player"))
+        self.assertTrue(game_object.set_round_score("draw"))
+        self.assertTrue(game_object.set_round_score("computer"))
+
+    def test_set_round_score_raise_error(self):
+        """Test the function set_round_score raise Error."""
+        # start game class
+        game_object = RockPaperScissor()
+        # test set score by winner invalid string
+        self.assertRaises(ValueError, game_object.set_round_score, "Stranger")
+        self.assertRaises(ValueError, game_object.set_round_score, "player ")
+        self.assertRaises(
+            ValueError, game_object.set_round_score, " computer ")
+        self.assertRaises(ValueError, game_object.set_round_score, " draw ")
+        self.assertRaises(ValueError, game_object.set_round_score, " ")
+        # try with None value
+        self.assertRaises(ValueError, game_object.set_round_score, None)
+        # try with number value
+        self.assertRaises(ValueError, game_object.set_round_score, 1)
+        # try with boolean
+        self.assertRaises(ValueError, game_object.set_round_score, True)
+        self.assertRaises(ValueError, game_object.set_round_score, False)
+
+    def test_get_game_winner(self):
+        """Test get game winner."""
+        # start game class
+        game_object = RockPaperScissor()
+        # Test case 1: player win
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        game_object.next_round()
+        game_object.set_round_score("player")
+        # assert the
+        self.assertEqual(
+            "player", game_object.get_game_winner())
+
+        # Test case 2: computer win
+        game_object.reset()
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("draw")
+        game_object.next_round()
+        game_object.set_round_score("draw")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        game_object.next_round()
+        game_object.set_round_score("draw")
+        game_object.next_round()
+        game_object.set_round_score("draw")
+        game_object.next_round()
+        game_object.set_round_score("player")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        game_object.next_round()
+        game_object.set_round_score("computer")
+        # assert the
+        self.assertEqual(
+            "computer", game_object.get_game_winner())
+
+    def test_print_round_result(self):
+        """Test print match round result."""
+        game_object = RockPaperScissor()
+
+        # Test case 1 print player win message
+        player, computer = "paper", "rock"
+        winner = "player"
+        game_object.set_round_score("player")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        player, computer = "scissor", "paper"
+        winner = "player"
+        game_object.set_round_score("player")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        player, computer = "rock", "scissor"
+        winner = "player"
+        game_object.set_round_score("player")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        # Test case 2 print draw message
+        player, computer = "scissor", "scissor"
+        winner = "draw"
+        game_object.set_round_score("draw")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        player, computer = "rock", "rock"
+        winner = "draw"
+        game_object.set_round_score("draw")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        player, computer = "paper", "paper"
+        winner = "draw"
+        game_object.set_round_score("draw")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+        game_object.next_round()
+
+        # Test case 3 computer win message
+        player, computer = "rock", "paper"
+        winner = "computer"
+        game_object.set_round_score("computer")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+
+        player, computer = "scissor", "rock"
+        winner = "computer"
+        game_object.set_round_score("computer")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
+
+        player, computer = "paper", "scissor"
+        winner = "computer"
+        game_object.set_round_score("computer")
+        self.assertTrue(game_object.print_round_result(
+            player, computer, winner))
 
 
 # main class for unit test
